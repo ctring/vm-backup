113e6c7de3cd5bc63714bba437719f106311d488
Cuong Nguyen <ctring23@gmail.com>
2017-09-04 13:19:26 -0400

    Turn constant to arguments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import time
 import glob
 import logging
+import argparse
 
 WORKING_DIR = os.path.abspath('test')
 WILDCARD = '*'
@@ -22,25 +23,31 @@ sh.setFormatter(formatter)
 logger.addHandler(sh)
 
 
+def expand_targets(targets):
+    vm = set()
+    for t in targets:
+        vm |= set(map(os.path.abspath, glob.glob(t)))
+    return vm
+
+
 def do_backup(vm):
-    logger.info('Backing up {}'.format(vm))
+    logger.info('Backing up %s', vm)
     for i in range(2):
-        logger.info('Progress... {}%'.format(50 * (i + 1)))
+        logger.info('Progress... %d%%', 50 * (i + 1))
         time.sleep(0.5)
 
 
-def main():
-    # Get absolute path to all vm files in the given working directory
-    existing_vm = glob.glob(os.path.join(WORKING_DIR, WILDCARD))
-    existing_vm = set(map(os.path.abspath, existing_vm))
-    logger.info('{} VMs found in {}'.format(len(existing_vm), WORKING_DIR))
-
+def consolidate_existing_and_backedup_vm(existing_vm, checkpoint_file, start_over):
     # all_vm contains existing VMs and possibly removed backed up VMs
     all_vm = existing_vm
 
+    if start_over:
+        logger.info('Starting over...')
+        return all_vm, set()
+
     backed_up = set()
-    if os.path.exists(CHECKPOINT_FILE):
-        with open(CHECKPOINT_FILE, 'r') as f:
+    if os.path.exists(checkpoint_file):
+        with open(checkpoint_file, 'r') as f:
             backed_up = f.readlines()
             backed_up = set([vm.strip() for vm in backed_up])
 
@@ -48,33 +55,43 @@ def main():
         removed_vm = backed_up.difference(all_vm)
         # Add backed-up-and-removed VMs to all_vm for accurate counts of backups
         all_vm = all_vm.union(backed_up)
-        
+       
         if len(backed_up) < len(all_vm):
             if len(removed_vm) > 0:
-                logger.info('Checkpoint file found. {}/{} VMs were backed up.'
-                            ' {} VMs were removed since last backup session.'
-                            ' Continuing...'
-                            .format(len(backed_up), len(all_vm), len(removed_vm)))
+                logger.info('Checkpoint file found. %d/%d VMs were backed up '
+                            '(%d VMs were removed since last backup session).'
+                            ' Continuing...',
+                            len(backed_up), len(all_vm), len(removed_vm))
             else:
-                logger.info('Checkpoint file found. {}/{} VMs were backed up.'
-                            ' Continuing...'.format(len(backed_up), len(all_vm)))
+                logger.info('Checkpoint file found. %d/%d VMs were backed up.'
+                            ' Continuing...', len(backed_up), len(all_vm))
         else:
             logger.info('Checkpoint file found. All VMs were backed up.'
                         ' Starting over...')
-            os.remove(CHECKPOINT_FILE)
+            os.remove(checkpoint_file)
             backed_up = set()
             all_vm = existing_vm
 
     else:
-        logger.info('Checkpoint file not found. Starting from the beginning')
-    
+        logger.info('Checkpoint file not found. Starting from the beginning...')
+
+    return all_vm, backed_up
+
+
+def main(targets, time_limit_s, checkpoint_file, start_over):
+    existing_vm = expand_targets(targets)
+    logger.info('Found %d VMs', len(existing_vm))
+
+    all_vm, backed_up = consolidate_existing_and_backedup_vm(
+        existing_vm, checkpoint_file, start_over)
+
     start_time = time.time()
     timed_out = False
     backed_up_this_run = 0
-    with open(CHECKPOINT_FILE, 'a') as f:
+    with open(checkpoint_file, 'a') as f:
         for vm in all_vm:
             current_time = time.time()
-            if current_time - start_time >= TIME_LIMIT_S:
+            if current_time - start_time >= time_limit_s:
                 timed_out = True
                 break
             if vm not in backed_up:
@@ -87,14 +104,31 @@ def main():
         logger.info('Time limit reached')
     else:
         logger.info('Finished backing up all VMs')
-        os.remove(CHECKPOINT_FILE)
-        logger.info('Removed checkpoint file {}'.format(CHECKPOINT_FILE))
+        os.remove(checkpoint_file)
 
-    logger.info('Backed up VMs in this run: {}'.format(
-        backed_up_this_run))
-    logger.info('Backed up VMs in total: {}/{}'.format(len(backed_up),
-        len(all_vm)))
+
+    logger.info('Backed up VMs in this run: %d', backed_up_this_run)
+    logger.info('Backed up VMs in total: %d/%d', len(backed_up), len(all_vm))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='This script backs up VMs in a specified time limit. '
+                    'It picks up from where it left off in last run.')
+    parser.add_argument('targets', nargs='+',
+                        help='one or more targets to be backed up. Unix-style '
+                             'pattern matching is available')
+    parser.add_argument('time_limit_s', type=int,
+                        help='backing up process will only runs within this '
+                             'time limit (in seconds)')
+    parser.add_argument('-cf', default='backup.chkpnt',
+                        help='name of checkpoint file (default: backup.chkpnt)')
+    parser.add_argument('-s', action='store_true',
+                        help='start the back up process from the beginning')
+
+    args = parser.parse_args()
+ 
+    main(targets=args.targets,
+         time_limit_s=args.time_limit_s,
+         checkpoint_file=args.cf,
+         start_over=args.s)
