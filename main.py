fa40115c342acae3c20b709e2c0acd53045fac62
Andrew Schade <schade.andrew.l@gmail.com>
2017-09-15 12:45:15 -0400

    Truly complete backup script with fixes to run on destination machine.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import subprocess
 
 # TODO implement argparse arguments.
 
-BACKUP_DIR = os.path.abspath('/media/andrew/Trancend/backup')
+BACKUP_DIR = os.path.abspath('/backup')
 WILDCARD = '*'
-CHECKPOINT_FILE = os.path.relpath('backup.chkpnt', BACKUP_DIR)
+CHECKPOINT_FILE = os.path.join(BACKUP_DIR,'backup.chkpnt')
 TIME_LIMIT_H = 1
 TIME_LIMIT_S = TIME_LIMIT_H*3600
 
@@ -40,11 +40,11 @@ def get_block_devs(vm_name):
     block_dev_cmd = ['virsh', 'domblklist', vm_name]
     block_dev_prc = subprocess.Popen(block_dev_cmd, stdout=subprocess.PIPE)
     devs = []
-    results = (block_dev_prc.communicate()[0]).split('\n')
+    results = (block_dev_prc.communicate()[0]).splitlines()
     for sect in results:
         if sect.endswith('qcow2'):
             devs.append(sect.split()[0])
-        LOGGER.info('Found block device {}'.format(sect))
+            print('Found block device {}'.format(sect))
     return devs
 
 def save_vm_state(vm_name, backup_dir=BACKUP_DIR):
@@ -61,42 +61,41 @@ def save_vm_state(vm_name, backup_dir=BACKUP_DIR):
     save_prc = subprocess.Popen(save_cmd)
     save_prc.communicate()
 
-    LOGGER.info('Backup of vm {} state successful'.format(vm_name))
+    print('Backup of vm {} state successful'.format(vm_name))
 
 def dump_vm_info(vm_name, backup_dir=BACKUP_DIR):
     """
     Saves the VM configuration XML files.
     """
     dump_name = os.path.join(backup_dir, '{}.xml'.format(vm_name))
-    LOGGER.info('Dumping config for {} to {}'.format(vm_name, dump_name))
+    print('Dumping config for {} to {}'.format(vm_name, dump_name))
     save_cmd = ['virsh',
                 'dumpxml',
                 '--security-info',
-                vm_name,
-                '>',
-                dump_name
+                vm_name
                ]
-    save_prc = subprocess.Popen(save_cmd)
-    save_prc.communicate()
+    with open(dump_name,'w+') as fout:
+        save_prc = subprocess.Popen(save_cmd, stdout=fout)
+        save_prc.communicate()
     
 
 def undefine_vm(vm_name):
     """Makes the VM transient"""
-    LOGGER.info('Destroying VM definitiion for VM {}'.format(vm_name))
+    print('Destroying VM definitiion for VM {}'.format(vm_name))
     cmd = ['virsh', 'undefine', vm_name]
     prc = subprocess.Popen(cmd)
     prc.communicate()
 
 def suspend_vm(vm_name):
     """Suspends the VM"""
-    LOGGER.info('Suspending VM {}'.format(vm_name))
+    print('Suspending VM {}'.format(vm_name))
     cmd = ['virsh', 'suspend', vm_name]
     prc = subprocess.Popen(cmd)
     prc.communicate()
 
 def restore_vm_state(vm_name, backup_dir=BACKUP_DIR):
     """Restores the VM. Necessary because save-state will stop the VM."""
-    LOGGER.info('Restoring VM {}'.format(vm_name))
+    print('Restoring VM {}'.format(vm_name))
     cmd = ['virsh',
            'restore',
            (os.path.join(backup_dir, vm_name+'-memory')),
@@ -107,7 +106,7 @@ def restore_vm_state(vm_name, backup_dir=BACKUP_DIR):
 
 def restore_vm_def(vm_name, backup_dir=BACKUP_DIR):
     """Restores the VM XML Configuration and defninition.."""
-    LOGGER.info('Restoring VM definition for VM {}'.format(vm_name))
+    print('Restoring VM definition for VM {}'.format(vm_name))
     cmd = ['virsh',
            'define',
            (os.path.join(backup_dir, vm_name+'.xml'))
@@ -115,8 +114,9 @@ def restore_vm_def(vm_name, backup_dir=BACKUP_DIR):
     prc = subprocess.Popen(cmd)
     prc.communicate()
 
-def safe_exit():
-    pass
+def safe_exit(vm_name):
+    for d in get_block_devs(vm_name):
+        subprocess.Popen(['virsh', 'blockjob', vm_name, d, '--abort'])
 
 def copy_block(vm_name, disk_name, backup_dir=BACKUP_DIR):
     """Copies the block device into backup."""
@@ -125,7 +125,7 @@ def copy_block(vm_name, disk_name, backup_dir=BACKUP_DIR):
                                                            disk_name
                                                           )
                               )
-    LOGGER.info('Copying disk {}, for vm {} into file {}...'.format(disk_name,
+    print('Copying disk {}, for vm {} into file {}...'.format(disk_name,
                                                                     vm_name,
                                                                     backup_name
                                                                    )
@@ -139,12 +139,13 @@ def copy_block(vm_name, disk_name, backup_dir=BACKUP_DIR):
     while progress < 100:
         progress_prc = subprocess.Popen(progress_cmd, stdout=subprocess.PIPE)
         progress_report, _ = progress_prc.communicate()
-        match = re.search(r'([0-9]{1,3})', progress_report)
-        if not match:
-            LOGGER.info("BlockJob aborted. Disk full?")
-            safe_exit()
-        progress = int(match.group(0))
-        LOGGER.info('Copying... {} %'.format(progress))
+        if '[' not in progress_report:
+            print("BlockJob aborted. Disk full?")
+            safe_exit(vm_name)
+            return
+        else:
+            progress = int(progress_report.split('[')[1].split()[0])
+            print('Copying... {} %'.format(progress))
         time.sleep(5)
 
 def get_backup_name(vm_name, bk_id, backup_dir=BACKUP_DIR):
@@ -166,7 +167,7 @@ def rotate_backups(vm_name):
         bk_prev = get_backup_name(vm_name, prev)
         bk = get_backup_name(vm_name, bkcnt)
         if os.path.isdir(bk_prev) and os.path.isdir(bk):
-            LOGGER.info('Remove old backup %s', bk_prev)
+            print('Remove old backup %s', bk_prev)
             cmd = ['mv',
                    bk_prev,
                    os.path.join(BACKUP_DIR, vm_name, 'to-be-removed')]
@@ -178,32 +179,32 @@ def rotate_backups(vm_name):
                         vm_name,
                         'to-be-removed',
                         vm_name+'.xml')):
-                LOGGER.info('Safely remove old backup directory.')
+                print('Safely remove old backup directory.')
                 cmd = ['rm',
                        '-rf',
                        os.path.join(BACKUP_DIR, vm_name, 'to-be-removed')]
                 cmd_prc = subprocess.Popen(cmd)
                 cmd_prc.communicate()
             else:
-                LOGGER.info('Cannot remove old backup directory. Not a backup.')
+                print('Cannot remove old backup directory. Not a backup.')
                 exit(1)
         if os.path.isdir(bk):
-            LOGGER.info('Rename %s to %s', bk, bk_prev)
+            print('Rename %s to %s', bk, bk_prev)
             cmd = ['mv', bk, bk_prev]
             cmd_prc = subprocess.Popen(cmd)
             cmd_prc.communicate()
         
         bkcnt -= 1
     backup_dir = get_backup_name(vm_name, 0)
-    os.mkdir(backup_dir)
-    subprocess.Popen(['chown', '-R', 'libvirt-qemu:kvm', basepath])
+    subprocess.Popen(['mkdir', '-p', backup_dir])
+    subprocess.Popen(['chown', '-R', 'root:kvm', basepath])
     return backup_dir
 
 
 def check_disk_space(vm_name):
     """Checks to see if the required disk space is free on the destination
     volume"""
-    LOGGER.info('Checking required disk space.')
+    print('Checking required disk space.')
     vm_req_space = 0
     disk_cmd = ['df', BACKUP_DIR]
     disk_prc = subprocess.Popen(disk_cmd, stdout=subprocess.PIPE)
@@ -215,32 +216,33 @@ def check_disk_space(vm_name):
         blk_space_prc = subprocess.Popen(blk_space_cmd, stdout=subprocess.PIPE)
         blk_space_out, _ = blk_space_prc.communicate()
         req_space = int(blk_space_out.split()[-1]) / 1024 # In KiBytes
-        LOGGER.info('Device {} requires {} KiB'.format(d, req_space))
+        print('Device {} requires {} KiB'.format(d, req_space))
         vm_req_space += req_space
 
     mem_size_cmd = ['virsh', 'dominfo', vm_name]
-    mem_size_prc = subprocess.Popen(mem_size_cmd, subprocess.PIPE)
+    mem_size_prc = subprocess.Popen(mem_size_cmd, stdout=subprocess.PIPE)
     mem_size_out, _ = mem_size_prc.communicate()
-    mem = int(mem_size_out.split('\n')[7].split(2))
-    LOGGER.info('VM memory size is {} KiB'.format(mem))
+    mem = int(mem_size_out.split('\n')[7].split()[2])
+    print('VM memory size is {} KiB'.format(mem))
     mem = int(mem*1.2)
-    LOGGER.info('VM memory requirement scaled to {} KiB'.format(mem))
+    print('VM memory requirement scaled to {} KiB'.format(mem))
     vm_req_space += mem
 
     last_backup_cmd = ['du', '-s', get_last_backup_name(vm_name)]
-    last_backup_prc = subprocess.Popen(last_backup_cmd, stdout=subprocess.PIPE)
+    last_backup_prc = subprocess.Popen(last_backup_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     last_backup_out, _ = last_backup_prc.communicate()
-    last_backup_size = int(last_backup_out.split()[0])
-    vm_req_space -= last_backup_size
+    if not _:
+        last_backup_size = int(last_backup_out.split()[0])
+        vm_req_space -= last_backup_size
 
     mb_free = disk_space / 1024
     mb_required = vm_req_space / 1024
 
     if mb_free < mb_required:
-        LOGGER.info('Cannot make backup; only {} MiB available.'.format(mb_free) +
+        print('Cannot make backup; only {} MiB available.'.format(mb_free) +
                     ' Minimum needed is {} MiB.'.format(mb_required))
         return 1
-    LOGGER.info(
+    print(
         'Backup possible: {} MiB expected to remain after backup'.format(
             mb_free-mb_required))
     return 0
@@ -248,10 +250,11 @@ def check_disk_space(vm_name):
 def backup_vm(vm_name):
     """Runs backup process for a specified vm."""
     subprocess.Popen(['mkdir', '-p', BACKUP_DIR])
-    subprocess.Popen(['chown', '-R', 'libvirt-qemu:kvm', BACKUP_DIR])
+    subprocess.Popen(['mkdir', '-p', os.path.join(BACKUP_DIR, vm_name)])
+    subprocess.Popen(['chown', '-R', 'root:kvm', BACKUP_DIR])
 
     # Starting Backup
-    LOGGER.info('Backup of %s started', vm_name)
+    print('Backup of %s started', vm_name)
     blocks = get_block_devs(vm_name)
     result = check_disk_space(vm_name)
     if result:
@@ -262,12 +265,13 @@ def backup_vm(vm_name):
     undefine_vm(vm_name)
     for d in blocks:
         copy_block(vm_name, d, backup_dir)
+        subprocess.Popen(['virsh', 'blockjob', vm_name, d, '--abort'])
     suspend_vm(vm_name)
     save_vm_state(vm_name, backup_dir)
     restore_vm_state(vm_name, backup_dir)
     restore_vm_def(vm_name, backup_dir)
 
-    LOGGER.info("Backup of %s finished", vm_name)
+    print("Backup of %s finished", vm_name)
 
 def do_backup(vm_name):
     """Runs backup code from ported script (original: https://goo.gl/SVCiq9)"""
@@ -298,14 +302,13 @@ def get_existing_vms(allow_inactive=False):
 def main():
     # Get absolute path to all vm files in the given working directory
     existing_vm = get_existing_vms()
-    existing_vm = set(map(os.path.abspath, existing_vm))
-    LOGGER.info('{} VMs found.'.format(len(existing_vm)))
+    print('{} VMs found.'.format(len(existing_vm)))
 
     # all_vm contains existing VMs and possibly removed backed up VMs
-    all_vm = existing_vm
+    all_vm = set(existing_vm)
 
     backed_up = set()
-    if os.path.exists(CHECKPOINT_FILE):
+    if os.path.isfile(CHECKPOINT_FILE):
         with open(CHECKPOINT_FILE, 'r') as openfile:
             backed_up = openfile.readlines()
             backed_up = set([vm.strip() for vm in backed_up])
@@ -317,22 +320,22 @@ def main():
         
         if len(backed_up) < len(all_vm):
             if len(removed_vm) > 0:
-                LOGGER.info('Checkpoint file found. {}/{} VMs were backed up.'
+                print('Checkpoint file found. {}/{} VMs were backed up.'
                             ' {} VMs were removed since last backup session.'
                             ' Continuing...'
                             .format(len(backed_up), len(all_vm), len(removed_vm)))
             else:
-                LOGGER.info('Checkpoint file found. {}/{} VMs were backed up.'
+                print('Checkpoint file found. {}/{} VMs were backed up.'
                             ' Continuing...'.format(len(backed_up), len(all_vm)))
         else:
-            LOGGER.info('Checkpoint file found. All VMs were backed up.'
+            print('Checkpoint file found. All VMs were backed up.'
                         ' Starting over...')
             os.remove(CHECKPOINT_FILE)
             backed_up = set()
             all_vm = existing_vm
 
     else:
-        LOGGER.info('Checkpoint file not found. Starting from the beginning')
+        print('Checkpoint file not found. Starting from the beginning')
    
     start_time = time.time()
     timed_out = False
@@ -350,15 +353,15 @@ def main():
                 openfile.write(vm + '\n')
 
     if timed_out:
-        LOGGER.info('Time limit reached')
+        print('Time limit reached')
     else:
-        LOGGER.info('Finished backing up all VMs')
+        print('Finished backing up all VMs')
         os.remove(CHECKPOINT_FILE)
-        LOGGER.info('Removed checkpoint file {}'.format(CHECKPOINT_FILE))
+        print('Removed checkpoint file {}'.format(CHECKPOINT_FILE))
 
-    LOGGER.info('Backed up VMs in this run: {}'.format(
+    print('Backed up VMs in this run: {}'.format(
         backed_up_this_run))
-    LOGGER.info('Backed up VMs in total: {}/{}'.format(len(backed_up),
+    print('Backed up VMs in total: {}/{}'.format(len(backed_up),
                                                        len(all_vm)))
 
 
