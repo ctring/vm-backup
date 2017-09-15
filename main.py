dbde6984510625500327b652bf34142bb0a50e6c
Andrew Schade <schade.andrew.l@gmail.com>
2017-09-15 10:56:28 -0400

    Finished (untested) sscript

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ CHECKPOINT_FILE = os.path.relpath('backup.chkpnt', BACKUP_DIR)
 TIME_LIMIT_H = 1
 TIME_LIMIT_S = TIME_LIMIT_H*3600
 
+MAX_BACKUPS = 2 # Maximum history of backups saved. 2 Means current backup and
+                # one previous.
+
 # Configuring logging object
 LOGGER = logging.getLogger('vm-backup')
 LOGGER.setLevel(logging.INFO)
@@ -44,7 +47,7 @@ def get_block_devs(vm_name):
         LOGGER.info('Found block device {}'.format(sect))
     return devs
 
-def save_vm_state(vm_name):
+def save_vm_state(vm_name, backup_dir=BACKUP_DIR):
     """
     Saves the VM Memory state, which requires quick suspend of
     the VM.
@@ -52,7 +55,7 @@ def save_vm_state(vm_name):
     save_cmd = ['virsh',
                 'save',
                 vm_name,
-                (os.path.join(BACKUP_DIR, vm_name+'-memory')),
+                (os.path.join(backup_dir, vm_name+'-memory')),
                 '--running'
                ]
     save_prc = subprocess.Popen(save_cmd)
@@ -60,11 +63,11 @@ def save_vm_state(vm_name):
 
     LOGGER.info('Backup of vm {} state successful'.format(vm_name))
 
-def dump_vm_info(vm_name):
+def dump_vm_info(vm_name, backup_dir=BACKUP_DIR):
     """
     Saves the VM configuration XML files.
     """
-    dump_name = os.path.join(BACKUP_DIR, '{}.xml'.format(vm_name))
+    dump_name = os.path.join(backup_dir, '{}.xml'.format(vm_name))
     LOGGER.info('Dumping config for {} to {}'.format(vm_name, dump_name))
     save_cmd = ['virsh',
                 'dumpxml',
@@ -91,23 +94,23 @@ def suspend_vm(vm_name):
     prc = subprocess.Popen(cmd)
     prc.communicate()
 
-def restore_vm_state(vm_name):
+def restore_vm_state(vm_name, backup_dir=BACKUP_DIR):
     """Restores the VM. Necessary because save-state will stop the VM."""
     LOGGER.info('Restoring VM {}'.format(vm_name))
     cmd = ['virsh',
            'restore',
-           (os.path.join(BACKUP_DIR, vm_name+'-memory')),
+           (os.path.join(backup_dir, vm_name+'-memory')),
            '--running'
           ]
     prc = subprocess.Popen(cmd)
     prc.communicate()
 
-def restore_vm_def(vm_name):
+def restore_vm_def(vm_name, backup_dir=BACKUP_DIR):
     """Restores the VM XML Configuration and defninition.."""
     LOGGER.info('Restoring VM definition for VM {}'.format(vm_name))
     cmd = ['virsh',
            'define',
-           (os.path.join(BACKUP_DIR, vm_name+'.xml'))
+           (os.path.join(backup_dir, vm_name+'.xml'))
           ]
     prc = subprocess.Popen(cmd)
     prc.communicate()
@@ -115,9 +118,9 @@ def restore_vm_def(vm_name):
 def safe_exit():
     pass
 
-def copy_block(vm_name, disk_name):
+def copy_block(vm_name, disk_name, backup_dir=BACKUP_DIR):
     """Copies the block device into backup."""
-    backup_name = os.path.join(BACKUP_DIR,
+    backup_name = os.path.join(backup_dir,
                                '{}-{}-backup.qcow2'.format(vm_name,
                                                            disk_name
                                                           )
@@ -144,9 +147,58 @@ def copy_block(vm_name, disk_name):
         LOGGER.info('Copying... {} %'.format(progress))
         time.sleep(5)
 
-def get_backup_name(vm_name, bk_id):
+def get_backup_name(vm_name, bk_id, backup_dir=BACKUP_DIR):
     """Composes the backup name."""
     return os.path.join(BACKUP_DIR, vm_name, 'backup-{}'.format(bk_id))
+
+def get_last_backup_name(vm_name):
+    """Returns the backup name for the final backup index"""
+    return get_backup_name(vm_name, MAX_BACKUPS - 1)
+
+def rotate_backups(vm_name):
+    """Rotates or deletes the last backup."""
+    basepath = os.path.join(BACKUP_DIR, vm_name)
+    older_backup = get_backup_name(vm_name, MAX_BACKUPS)
+    bkcnt = MAX_BACKUPS -2
+
+    while bkcnt > 0:
+        prev = bkcnt+1
+        bk_prev = get_backup_name(vm_name, prev)
+        bk = get_backup_name(vm_name, bkcnt)
+        if os.path.isdir(bk_prev) and os.path.isdir(bk):
+            LOGGER.info('Remove old backup %s', bk_prev)
+            cmd = ['mv',
+                   bk_prev,
+                   os.path.join(BACKUP_DIR, vm_name, 'to-be-removed')]
+            cmd_prc = subprocess.Popen(cmd)
+            cmd_prc.communicate()
+            if os.path.isfile(
+                    os.path.join(
+                        BACKUP_DIR,
+                        vm_name,
+                        'to-be-removed',
+                        vm_name+'.xml')):
+                LOGGER.info('Safely remove old backup directory.')
+                cmd = ['rm',
+                       '-rf',
+                       os.path.join(BACKUP_DIR, vm_name, 'to-be-removed')]
+                cmd_prc = subprocess.Popen(cmd)
+                cmd_prc.communicate()
+            else:
+                LOGGER.info('Cannot remove old backup directory. Not a backup.')
+                exit(1)
+        if os.path.isdir(bk):
+            LOGGER.info('Rename %s to %s', bk, bk_prev)
+            cmd = ['mv', bk, bk_prev]
+            cmd_prc = subprocess.Popen(cmd)
+            cmd_prc.communicate()
+        
+        bkcnt -= 1
+    backup_dir = get_backup_name(vm_name, 0)
+    os.mkdir(backup_dir)
+    subprocess.Popen(['chown', '-R', 'libvirt-qemu:kvm', basepath])
+    return backup_dir
+
 
 def check_disk_space(vm_name):
     """Checks to see if the required disk space is free on the destination
@@ -154,13 +206,72 @@ def check_disk_space(vm_name):
     LOGGER.info('Checking required disk space.')
     vm_req_space = 0
     disk_cmd = ['df', BACKUP_DIR]
+    disk_prc = subprocess.Popen(disk_cmd, stdout=subprocess.PIPE)
+    disk_out, _ = disk_prc.communicate()
+    disk_space = int(disk_out.split()[9])
+
+    for d in get_block_devs(vm_name):
+        blk_space_cmd = ['virsh', 'domblkinfo', vm_name, d]
+        blk_space_prc = subprocess.Popen(blk_space_cmd, stdout=subprocess.PIPE)
+        blk_space_out, _ = blk_space_prc.communicate()
+        req_space = int(blk_space_out.split()[-1]) / 1024 # In KiBytes
+        LOGGER.info('Device {} requires {} KiB'.format(d, req_space))
+        vm_req_space += req_space
+
+    mem_size_cmd = ['virsh', 'dominfo', vm_name]
+    mem_size_prc = subprocess.Popen(mem_size_cmd, subprocess.PIPE)
+    mem_size_out, _ = mem_size_prc.communicate()
+    mem = int(mem_size_out.split('\n')[7].split(2))
+    LOGGER.info('VM memory size is {} KiB'.format(mem))
+    mem = int(mem*1.2)
+    LOGGER.info('VM memory requirement scaled to {} KiB'.format(mem))
+    vm_req_space += mem
+
+    last_backup_cmd = ['du', '-s', get_last_backup_name(vm_name)]
+    last_backup_prc = subprocess.Popen(last_backup_cmd, stdout=subprocess.PIPE)
+    last_backup_out, _ = last_backup_prc.communicate()
+    last_backup_size = int(last_backup_out.split()[0])
+    vm_req_space -= last_backup_size
+
+    mb_free = disk_space / 1024
+    mb_required = vm_req_space / 1024
+
+    if mb_free < mb_required:
+        LOGGER.info('Cannot make backup; only {} MiB available.'.format(mb_free) +
+                    ' Minimum needed is {} MiB.'.format(mb_required))
+        return 1
+    LOGGER.info(
+        'Backup possible: {} MiB expected to remain after backup'.format(
+            mb_free-mb_required))
+    return 0
+
+def backup_vm(vm_name):
+    """Runs backup process for a specified vm."""
+    subprocess.Popen(['mkdir', '-p', BACKUP_DIR])
+    subprocess.Popen(['chown', '-R', 'libvirt-qemu:kvm', BACKUP_DIR])
+
+    # Starting Backup
+    LOGGER.info('Backup of %s started', vm_name)
+    blocks = get_block_devs(vm_name)
+    result = check_disk_space(vm_name)
+    if result:
+        exit(1)
+    
+    backup_dir = rotate_backups(vm_name)
+    dump_vm_info(vm_name, backup_dir)
+    undefine_vm(vm_name)
+    for d in blocks:
+        copy_block(vm_name, d, backup_dir)
+    suspend_vm(vm_name)
+    save_vm_state(vm_name, backup_dir)
+    restore_vm_state(vm_name, backup_dir)
+    restore_vm_def(vm_name, backup_dir)
+
+    LOGGER.info("Backup of %s finished", vm_name)
 
 def do_backup(vm_name):
     """Runs backup code from ported script (original: https://goo.gl/SVCiq9)"""
-    LOGGER.info('Backing up {}'.format(vm_name))
-    for i in range(2):
-        LOGGER.info('Progress... {}%'.format(50 * (i + 1)))
-        time.sleep(0.5)
+    backup_vm(vm_name)
 
 def parse_list(vm_list):
     """Uses the format specification at https://goo.gl/p8F6Jv to get
